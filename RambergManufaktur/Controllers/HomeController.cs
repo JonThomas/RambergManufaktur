84cc85c3d86b984722d4474cd6d17fc26fbcc0e8
Jon-Thomas Eliassen <jonel@samfundet.no>
2014-04-16 11:18:56 +0200

    Første versjon

---
+++ RambergManufaktur/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RambergManufaktur.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Ramberg Manufaktur er eneste forhandler av klær og gardiner vest for Nappstraumen.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Ta gjerne kontakt dersom det er noe du lurer på.";
+
+            return View();
+        }
+    }
+}
